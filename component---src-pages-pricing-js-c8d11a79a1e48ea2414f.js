3e6156f8d58aee1476297fb44b4208b861d71075
sinamoraddar <sinamoraddar@gmail.com>
2019-12-11 12:40:56 +0330

    Updates

---
+++ component---src-pages-pricing-js-c8d11a79a1e48ea2414f.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{u52P:function(e,n,t){"use strict";t.r(n);var i=t("q1tI"),l=t.n(i),a=t("Bl7J"),c=function(){return l.a.createElement("div",null,"this is the pricing")},r=t("vrFN");n.default=function(){return l.a.createElement(a.a,null,l.a.createElement(r.a,{title:"Pricing component with toggle coding challenge"}),l.a.createElement(c,null))}}}]);
+//# sourceMappingURL=component---src-pages-pricing-js-c8d11a79a1e48ea2414f.js.map
